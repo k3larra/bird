e0c1b0ce47500e54738e8a5d525ffa4dcb4f8924
Lars Holmberg <lars.holmberg@mah.se>
2023-11-29 22:49:54 +0100

    Added ml_code

--- flask/birdweb/.venv/birdnest.py
+++ flask/birdweb/.venv/birdnest.py
@@ -1,14 +1,14 @@
-import os
-import sys
-import datetime
 from flask import Flask
 from flask import request
 import json
-import shutil
 app = Flask(__name__)
+##
+
 #firebase
 import firebase_admin
 from firebase_admin import credentials, db
+from ml_code import train_and_save, getResNet50_model
+import asyncio
 
 # Initialize the Firebase Admin SDK
 cred = credentials.Certificate('../../secrets/bird-ad15f-firebase-adminsdk-hzlhg-4ccf1a7271.json')
@@ -19,7 +19,7 @@ firebase_admin.initialize_app(cred, {
 
 @app.route("/")
 def hello_world():
-    print("Hejlo World")
+    print("Hello World")
     return "<p>Hello, World!</p>"
 
 @app.route('/retrain', methods=['GET'])
@@ -29,22 +29,34 @@ def retrain():
     return json.dumps({"functioning":0 })
 
 @app.route('/json_endpoint', methods=['POST'])
-def json_endpoint():
+
+async def train_and_save_async(*args):
+    return train_and_save(*args)
+
+async def json_endpoint():
     data = request.get_json()
+    num_epochs = 1
+    annotation_json_file = data["images"]
+    image_path_resized = '../ottenbyresized'
+    save_path = "../models"
+
     print(data["training_set_ref"])
     print(data["images"][10])
     print(data["uid"])
     print(data["ml_model"])
     print(data["training_set_ref"])
-    #Start the training of a Resnet50 model
-    
-
     ref = db.reference('/').child(data["uid"]).child("metadata").child(data["training_set_ref"]).update({
         'ml_train': False,
         'ml_train_ongoing': True,
         'ml_train_status': "running",
-        'ml_training_started_timestamp':{".sv": "timestamp"}
+        'ml_training_started_timestamp': {".sv": "timestamp"}
     }) 
+
+    # Start the training of a Resnet50 model asynchronously
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, train_and_save_async, *getResNet50_model(), num_epochs, annotation_json_file, image_path_resized, save_path)
+    print(result)
+
     return json.dumps({"status":"running" })
 
 #app.run()
---
+++ flask/birdweb/.venv/ml_code.py
@@ -0,0 +1,104 @@
+import os
+import json
+import uuid
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from pathlib import Path
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+from torchvision.io import ImageReadMode
+from torch.optim import lr_scheduler
+from torchvision.models import efficientnet_v2_s, EfficientNet_V2_S_Weights, resnet50, ResNet50_Weights
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+annotation_json_file = './json_file.json'
+image_path_resized = '../ottenbyresized'
+save_path='./'
+batch_size = 32
+num_epochs=2
+
+with open(annotation_json_file,encoding='utf-8') as f:
+    data = json.load(f)
+    data = data['images']
+labels = [item['concept'] for item in data] 
+categories = np.unique(labels).tolist()
+def label_to_idx(label):
+    return categories.index(label)
+
+def idx_to_label(idx):
+    return categories[idx]
+
+class CustomImageDataset(Dataset):
+    def __init__(self, json_file, image_path, transform=None, target_transform=None):
+        with open(json_file,encoding='utf-8') as f:
+            data = json.load(f)
+            self.img_labels = data['images']
+        for index in range(len(self.img_labels)):          #For windows
+           self.img_labels[index]['image_location'] = Path(self.img_labels[index]['image_location'])  # Replace backslashes with forward slashes
+        #for index in range(len(self.img_labels)):          #For linux
+        #    self.img_labels[index]['image_location'] = self.img_labels[index]['image_location'].replace("\\", "/")  # Replace backslashes with forward slashes
+        self.image_path = image_path
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_path, self.img_labels[idx]['image_location'])
+        image = read_image(img_path, ImageReadMode.UNCHANGED)
+        image = image.float()
+        image /= 255.
+        label = self.img_labels[idx]['concept'] 
+        label = torch.tensor(label_to_idx(label))
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+def getEfficientNet_V2_S_model():
+    weights = EfficientNet_V2_S_Weights.DEFAULT
+    model_transforms = weights.transforms()
+    model = efficientnet_v2_s(weights=weights)
+    model._name="EfficientNet_V2_S"
+    model.eval()
+    return model, model_transforms
+
+def getResNet50_model():
+    weights = ResNet50_Weights.DEFAULT
+    model_transforms = weights.transforms()
+    model = resnet50(weights=weights)
+    model._name="ResNet50"
+    model.eval()
+    return model, model_transforms
+
+def train_model(model, criterion, optimizer,training_loader):
+    model.train()  
+    for inputs, labels in training_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+def train_and_save(model,model_transforms,num_epochs,annotation_json_file, image_path_resized,save_path):
+    bird_dataset = CustomImageDataset(annotation_json_file, image_path_resized, transform=model_transforms, target_transform=None)
+    training_loader = DataLoader(bird_dataset, batch_size=batch_size, shuffle=True)  #32,64
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    for epoch in range(num_epochs):
+        train_model(model, criterion, optimizer_ft,training_loader)
+        scheduler.step()
+    model_file_name = str(uuid.uuid4())+model._name+'.pth'
+    torch.save(model, os.path.join(save_path,model_file_name))
+    return model_file_name
+
+#print(train_and_save(*getResNet50_model(),num_epochs,annotation_json_file, image_path_resized,save_path))
