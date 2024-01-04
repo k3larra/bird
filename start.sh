8684c78e2b7428e7055cdd3f95fab3161abfddd2
Lars Holmberg <lars.holmberg@mah.se>
2024-01-04 15:23:05 +0100

    Change start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# Navigate to your project directory
 cd ~/ottenby/bird/flask/birdweb/
-# Activate the virtual environment
 source .venv/bin/activate
-# Set the FLASK_APP environment variable
 export FLASK_APP=birdnest
 # Optionally, enable debug mode
 export FLASK_ENV=development
 # Start the Flask server
-flask run --debugger &
-#flask run --debugger > flask.log 2>&1 &
-#Start node server
+#flask run --debugger &
+flask run --debugger > flask.log 2>&1 &
+#Start node server fir connecting to bird firebase and flask
 cd ~/ottenby/bird/node/
-#node nodebird.js > node.log 2>&1 &
-node nodebird.js &
-## Sar
+node nodebird.js > node_firebase_flask.log 2>&1 &
+#node nodebird.js &
+## cd
+cd ~/ottenby/
+# Start the bird server
+node server.js > node_server.log 2>&1 &
+# to run this file:
+#chmod +x start.sh
+#./start.sh
