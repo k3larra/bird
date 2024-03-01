b169fa26fb0cde43a993159ca09ae7de9ecf9d1b
Lars Holmberg <lars.holmberg@mah.se>
2024-03-01 08:44:59 +0100

    Added venv start

---
+++ start_venv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd ~/bird
+git checkout -- start_venv.sh
+git update-index --assume-unchanged start_venv.sh
+git pull origin main 
+# cd ~/ottenby/bird/flask/birdweb/
+cd ~/bird/flask/birdweb/
+source .venv/bin/activate
+export FLASK_APP=birdnest
+# Optionally, enable debug mode
+export FLASK_ENV=development
+# Start the Flask server
+#flask run --debugger &
+flask run --debugger > flask.log 2>&1 &
+#Start node server fir connecting to bird firebase and flask
+# cd ~/ottenby/bird/node/
+cd ~/bird/node
+node nodebird.js > node_firebase.log 2>&1 &
+#node nodebird.js &
+## cd
+# cd ~/ottenby/bird
+cd ~/bird
+# Start the web server
+authbind node server.js > node_server.log 2>&1 &
+#authbind node server.js &
+#sudo node server.js &
+# to run this file:
+#chmod +x start.sh
+#./start.sh
