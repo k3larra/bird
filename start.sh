3c57e0c588f3f748f2157b86dbfcca370860c24e
Lars Holmberg <lars.holmberg@mah.se>
2024-04-12 10:34:32 +0200

    chown

--- start.sh
+++ start.sh
@@ -5,23 +5,16 @@ git update-index --assume-unchanged start.sh
 git pull origin main 
 cd /home/lars_rauer/bird/flask/birdweb/
 source .venv/bin/activate
+# Start the Flask server
 export FLASK_APP=birdnest
 export FLASK_ENV=development
-# Start the Flask server
-#flask run --debugger &
 flask run > flask.log 2>&1 &
-#Start node server fir connecting to bird firebase and flask
-# cd ~/ottenby/bird/node/
+#Start node server fir connecting to bird firebase and flask server
 cd /home/lars_rauer/bird/node
 node nodebird.js > node_firebase.log 2>&1 &
-#node nodebird.js &
-## cd
-# cd ~/ottenby/bird
 cd /home/lars_rauer/bird
 # Start the web server
 authbind node server.js > node_server.log 2>&1 &
-#authbind node server.js &
-#sudo node server.js &
 # to run this file:
 #chmod +x start.sh
 #./start.sh
