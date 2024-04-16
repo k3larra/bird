079ddba10c6bace4fdf81b164c089db0eecefcd0
Lars Holmberg <lars.holmberg@mah.se>
2024-04-16 11:34:19 +0200

    check..

--- systemd/teststart_services.sh
+++ systemd/teststart_services.sh
@@ -4,19 +4,19 @@
 # Then run this script to start the services
 # Then check the status of the services with systemctl status <service_name>
 # Start the git pull service
-sudo systemctl start git_pull.service
+sudo systemctl start autostart_gitupdate.service
 # Check the status of the git pull service
-sudo systemctl status git_pull.service
+sudo systemctl status autostart_gitupdate.service
 # Start the flask server service
-sudo systemctl start start_flask.service
+sudo systemctl start autostart_flask.service
 # Check the status of the flask server service
-sudo systemctl status start_flask.service
+sudo systemctl status autostart_flask.service
 # Start the firebase to flask server service
-sudo systemctl start start_firebase_to_flask.service
+sudo systemctl start autostart_firebase_flask.service
 # Check the status of the firebase to flask server service
-sudo systemctl status start_firebase_to_flask.service
+sudo systemctl status autostart_firebase_flask.service
 # Start the web server service
-sudo systemctl start start_web_server.service
+sudo systemctl start autostart_webserver.service
 # Check the status of the web server service
-sudo systemctl status start_web_server.service
+sudo systemctl status autostart_webserver.service
 echo "All services started and checked"
